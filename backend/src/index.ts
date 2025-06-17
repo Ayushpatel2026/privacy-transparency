59081213cfd32ba6e22094c07784cb77efb30307
Ayushpatel2026 <ayushpatel.2026@gmail.com>
2025-06-17 09:03:21 -0400

    set up node backend

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+// helps to parse JSON data
+app.use(express.json());
+
+// helps to parse urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+
+// allows cross-origin requests
+app.use(cors());
+
+// test endpoint
+app.get('/api/test', async (req : Request, res : Response) => {
+  res.json({message: 'Hello from the backend!'})
+});
+
+// start the server
+app.listen(7000, () => {
+    console.log('Server is running on http://localhost:7000');
+})
