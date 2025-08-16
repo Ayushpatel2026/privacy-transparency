38256e3f2f1d9704d622ce18b9f652a3c09f3ba1
Ayushpatel2026 <ayushpatel.2026@gmail.com>
2025-08-16 16:14:18 -0400

    fix: deployment error

--- backend/src/lib/cron.ts
+++ backend/src/lib/cron.ts
@@ -1,9 +1,9 @@
-import cron from "cron";
+import { CronJob } from "cron";
 import https from "https";
 
 // This cron job runs every 14 minutes to ping the API URL
 // this is because Render's free tier services go to sleep after 15 minutes of inactivity
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
     https.get(process.env.API_URL!, (res) => {
         if (res.statusCode === 200) {
             console.log("Cron job executed successfully at", new Date());
